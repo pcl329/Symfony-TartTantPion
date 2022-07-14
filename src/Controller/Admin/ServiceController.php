fa43b32307d4cd53197fd3e1c4680b5164dd4826
PichunLEE <pc.lee.simplon@gmail.com>
2022-07-14 15:49:26 +0200

    updated service and final modification

--- src/Controller/Admin/ServiceController.php
+++ src/Controller/Admin/ServiceController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/service')]
+#[Route('/admin/service')]
 class ServiceController extends AbstractController
 {
     #[Route('/', name: 'app_service_index', methods: ['GET'])]
